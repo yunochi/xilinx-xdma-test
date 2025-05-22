ab162bcf079f32a59c034a411c16f56367bcb15e
Yuri Lee <yrlee@mycreatz.com>
2025-05-22 13:31:35 +0900

    config bar tool 수정

--- config_bar_test.c
+++ config_bar_test.c
@@ -11,19 +11,20 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 #include "user_config.h"
+#include <getopt.h>
 
+#define COUNTER_NORMAL 0b00
+#define COUNTER_DOWN 0b01
+#define CONTER_STOP 0b10
+#define RANDOM_DATA 0b11
 
-#define COUNTER_STOP 1 << 0
-#define COUNTER_DOWN 1 << 1
+const char MODES[][30] = {"up", "down", "stop", "random"};
 
 int main(int argc, char **argv)
 {
     int fd;
-    bool counter_stop;
-    bool counter_down_mode;
-    uint64_t counter_step;
-
     printf("AXI-ST Config BAR \nAXI-Stream Data generator Config: \n");
+
     fd = open(USER_CONFIG_BAR, O_RDWR | O_SYNC);
     if (fd == -1)
     {
@@ -36,26 +37,44 @@ int main(int argc, char **argv)
         perror("Mamory Map");
         exit(-1);
     }
-    if (argc == 4) {
-        uint32_t reg0, reg1, reg2;
-        counter_stop = strcmp(argv[1], "1") == 0;
-        counter_down_mode = strcmp(argv[2], "1") == 0;
-        counter_step = strtoull(argv[3], NULL, 10);
-        reg0 = counter_down_mode << 1 | counter_stop;
-        reg1 = counter_step >> 32;
-        reg2 = counter_step;
-        memcpy(map, &reg0, 4);
-        memcpy(map+4, &reg1, 4);
-        memcpy(map+8, &reg2, 4);
 
+    char c;
+    while ((c = getopt(argc, argv, "m:s:h")) != -1)
+    {
+        switch (c)
+        {
+        case 'm':
+            for(int i=0;;i++) {
+                if(i == sizeof(MODES)/sizeof(MODES[0])) {
+                    printf("Invalid Data Mode!\n"); 
+                    exit(-1);
+                }
+                else if(strcmp(optarg, MODES[i]) == 0) {
+                    uint8_t data_mode;
+                    data_mode = i;
+                    *(uint8_t*)map = data_mode;
+                    printf("SET Data Mode to %s\n", MODES[i]);
+                    break;
+                }
+            }
+            break;
+        case 's':
+            uint64_t counter_step;
+            counter_step = strtoull(optarg, NULL, 10);
+            memcpy(map+4, &counter_step, 8);
+            printf("Set Counter Step to %lu\n", counter_step);
+            break;
+        case 'h':
+        case '?':
+            printf(" -m Data Mode ('up', 'down', 'stop', 'random')\n -s Counter Step \n");
+            exit(0);
+            break;
+        }
     }
 
-    counter_stop = (*(uint8_t *)map & COUNTER_STOP) != 0;
-    counter_down_mode = (*(uint8_t *)map & COUNTER_DOWN) != 0;
-    counter_step = *(uint32_t *)((char *)map + 4);
-    counter_step =  counter_step << 32 | *(uint32_t *)((char *)map + 8);
-
-    printf("Counter Stop? %d.  ", counter_stop);
-    printf("Counter Down? %d.  ", counter_down_mode);
-    printf("Counter Step %lu + 1. \n", counter_step);
+    uint64_t step;
+    memcpy(&step, map+4, 8);
+    uint8_t mode = *(uint8_t*)map;
+    printf("Data Mode '%s' \n", MODES[mode]);
+    printf("Counter Step %lu. \n", step);
 }
